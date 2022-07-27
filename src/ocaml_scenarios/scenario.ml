8a02341ddacd37a23bf24562b36f18b66e07583f
Quyen <quyen@marigold.dev>
2022-07-27 16:29:30 +0200

    ill-type input when deploy the consensus contract

--- src/ocaml_scenarios/scenario.ml
+++ src/ocaml_scenarios/scenario.ml
@@ -20,33 +20,61 @@ open Consensus_ocaml_interface
 open Tzfunc.Rp
 open Factori_types
 
-(* Using the info from node/0/identity.json *)
-let key_hash = "tz1cQHF5EpGrGWt3fdYLJrWmNZBVw2oKqPwY"
+(* Using the info from node/i/identity.json
+   - key_hash_0 : data/0 and so on
+*)
+let key_hash_0 = "tz1cQHF5EpGrGWt3fdYLJrWmNZBVw2oKqPwY"
+let key_hash_1 = "tz1W3vJkX7kfbp8Y4RfdTmgQwV1Gm5cwcPHU"
+let key_hash_2 = "tz1Zhp7V6VAH5Fvc3Q8To92tJXWfyvu9FRN5"
+let key_hash_3 = "tz1TJ4PxhKwumAqScGyJYBkUdo2U2PJRB49Y"
 
 let uri = "http://localhost:4440"
 
 let nonce = Z.zero
 
-let discovery_storage = Literal [(key_hash, (nonce,uri))]
+let discovery_storage = Literal [(key_hash_0, (nonce,uri))]
 
 (* This discovery_kt1 address is deployed in the ithaca_node 
    provided by factori lib *)
 let discovery_kt1 = "KT1Fp4eEdKcugr8Ftf19UBdMTnyCsTut3LtR"
 
+let empty_bytes = Crypto.H.mk ""
+
+let ticket_bytes = Ticket empty_bytes
+
+let vault_storage = {
+  vault = Literal [("", empty_bytes), ticket_bytes];
+  used_handles = Literal [(Z.zero, ())];
+  known_handles_hash = Literal [(empty_bytes, ())]
+}
+
+let root_hash = {
+  current_validators = [key_hash_0; key_hash_1; key_hash_2; key_hash_3];
+  current_state_hash  = empty_bytes;
+  current_handles_hash = empty_bytes;
+  current_block_height = Z.zero;
+  current_block_hash = empty_bytes 
+}
+
+let consensus_storage = {
+  vault = vault_storage; 
+  root_hash = root_hash
+}
+
 let main () =
   let _ = Tzfunc.Node.set_silent true in 
   Format.printf "Calling discovery contract@.";
   Format.printf "KT1: %s@." discovery_kt1;
-  (*Format.printf "Deploying the contract@.";
-    let>? discovery_kt1, _op_hash =
-    deploy 
+  Format.printf "Deploying the contract@.";
+    let>? consensus_kt1, _op_hash =
+    Consensus_ocaml_interface.deploy 
     ~node:Blockchain.ithaca_node
-    ~name:"discovery"
+    ~name:"consensus"
     ~from:Blockchain.alice_flextesa
     ~amount:10000L
-    discovery_storage  
+    consensus_storage  
   in 
-  Format.printf "KT1: %s@." discovery_kt1;*)
+  Format.printf "KT1: %s@." consensus_kt1;
 
   Lwt.return_ok ()
   
